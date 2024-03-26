f1b3a2ae1a3c5e193f0dd6713a3d4b2289cf95a1
PacDu <flagada30du30@gmail.com>
2024-03-26 10:51:02 +0100

    feat: ajout des réponses dans le contrôleur générique

--- src/Controleur/ControleurGenerique.php
+++ src/Controleur/ControleurGenerique.php
@@ -2,51 +2,45 @@
 
 namespace App\Trellotrolle\Controleur;
 
+use App\Trellotrolle\Lib\Conteneur;
 use App\Trellotrolle\Lib\MessageFlash;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGenerator;
 
 class ControleurGenerique {
 
-    protected static function afficherVue(string $cheminVue, array $parametres = []): void
+    protected static function afficherVue(string $cheminVue, array $parametres = []): Response
     {
         extract($parametres);
 //        $messagesFlash = $_REQUEST["messagesFlash"] ?? [];
         $messagesFlash = MessageFlash::lireTousMessages();
+        ob_start();
         require __DIR__ . "/../vue/$cheminVue";
+        $corpsReponse = ob_get_clean();
+        return new Response($corpsReponse);
     }
 
     // https://stackoverflow.com/questions/768431/how-do-i-make-a-redirect-in-php
-    protected static function redirection(string $controleur = "", string $action = "", array $query = []) : void
+    protected static function redirection(string $route, array $parametres = []): RedirectResponse
     {
-        $queryString = [];
-        if ($action != "") {
-            $queryString[] = "action=$action";
-        }
-        if ($controleur != "") {
-            $queryString[] = "controleur=$controleur";
-        }
-        foreach ($query as $name => $value) {
-            $name = rawurlencode($name);
-            $value = rawurlencode($value);
-            $queryString[] = "$name=$value";
-        }
-        $url = "Location: ./controleurFrontal.php?" . join("&", $queryString);
-        header($url);
-        exit();
+        /** @var UrlGenerator $generateurUrl */
+        $generateurUrl = Conteneur::recupererService("generateurUrl");
+        $url = $generateurUrl->generate($route, $parametres);
+
+        return new RedirectResponse($url, Response::HTTP_FOUND);
     }
 
-    public static function afficherErreur($messageErreur = "", $controleur = ""): void
+    public static function afficherErreur($messageErreur = "", $statusCode = 400): Response
     {
-        $messageErreurVue = "Problème";
-        if ($controleur !== "")
-            $messageErreurVue .= " avec le contrôleur $controleur";
-        if ($messageErreur !== "")
-            $messageErreurVue .= " : $messageErreur";
-
-        ControleurGenerique::afficherVue('vueGenerale.php', [
+        $reponse = ControleurGenerique::afficherVue('vueGenerale.php', [
             "pagetitle" => "Problème",
             "cheminVueBody" => "erreur.php",
-            "messageErreur" => $messageErreurVue
+            "messageErreur" => $messageErreur
         ]);
+
+        $reponse->setStatusCode($statusCode);
+        return $reponse;
     }
 
     public static function issetAndNotNull(array $requestParams) : bool {
