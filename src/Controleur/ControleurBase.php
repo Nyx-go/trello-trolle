5f8043409d789bb067c4c9a71cde86174bb5011a
gillet <paco.gillet@etu-umontpellier.fr>
2024-03-26 15:19:44 +0100

    fix: renommage de la route par défaut

--- src/Controleur/ControleurBase.php
+++ src/Controleur/ControleurBase.php
@@ -7,7 +7,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ControleurBase extends ControleurGenerique
 {
-    #[Route(path: '/accueil', name:'default', methods:["GET"])]
+    #[Route(path: '/', name:'default', methods:["GET"])]
     public static function accueil(): Response
     {
         return ControleurBase::afficherVue('vueGenerale.php', [
