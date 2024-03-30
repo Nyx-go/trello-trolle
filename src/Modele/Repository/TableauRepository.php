b734abbaee03f3de8ced252be5112a9586576872
Paco Gillet <paco.gillet@etu.umontpellier.fr>
2024-03-30 18:23:35 +0100

    fix

--- src/Modele/Repository/TableauRepository.php
+++ src/Modele/Repository/TableauRepository.php
@@ -67,7 +67,7 @@ class TableauRepository extends AbstractRepository
     public function estParticipant($idTableau, $login) : bool{
         $sql = "SELECT login FROM participe WHERE idtableau =:idTableau";
         $pdoStatement = ConnexionBaseDeDonnees::getPdo()->prepare($sql);
-        $pdoStatement->execute(["idTableau" => $idTableau]);
+        $pdoStatement->execute(["idtableau" => $idTableau]);
         $obj = $pdoStatement->fetch();
         foreach ($obj as $item) {
             if ($item === $login) return true;
