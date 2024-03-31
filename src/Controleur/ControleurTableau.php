f36da755626334f89e270de979bdb06bd5a28bb7
Paco Gillet <paco.gillet@etu.umontpellier.fr>
2024-03-31 17:17:13 +0200

    fix: participants tableau

--- src/Controleur/ControleurTableau.php
+++ src/Controleur/ControleurTableau.php
@@ -52,19 +52,6 @@ class ControleurTableau extends ControleurGenerique
              * @var Carte[] $cartes
              */
             $cartes = $carteRepository->recupererCartesColonne($colonne->getIdColonne());
-//            foreach ($cartes as $carte) {
-//                $affectations = (new AffecteRepository())->recupererParIdCarte($carte->getIdCarte());
-//                foreach ($affectations as $affectation) {
-//                    $utilisateur = (new UtilisateurRepository())->recupererParClePrimaire(array("login"=>$affectation->getLogin()));
-//                    if(!isset($participants[$utilisateur->getLogin()])) {
-//                        $participants[$utilisateur->getLogin()] = ["infos" => $utilisateur, "colonnes" => []];
-//                    }
-//                    if(!isset($participants[$utilisateur->getLogin()]["colonnes"][$colonne->getIdColonne()])) {
-//                        $participants[$utilisateur->getLogin()]["colonnes"][$colonne->getIdColonne()] = [$colonne->getTitreColonne(), 0];
-//                    }
-//                    $participants[$utilisateur->getLogin()]["colonnes"][$colonne->getIdColonne()][1]++;
-//                }
-//            }
             $data[] = $cartes;
         }
 
